7c6fbf8377532cccb5f5e3b550c430324e89c59e
SemBauke <semboot699@gmail.com>
2023-02-10 18:31:47 +0100

    feat: remember steps by date

--- lib/services/step_storage_service.dart
+++ lib/services/step_storage_service.dart
@@ -1,1 +1,48 @@
-class StepStorageService {}
+import 'package:shared_preferences/shared_preferences.dart';
+
+class StepStorageService {
+  String _todaySteps = '0';
+  String get todaySteps => _todaySteps;
+
+  set setTodaysSteps(String steps) {
+    _todaySteps = steps;
+  }
+
+  Future<void> init() async {
+    setTodaysSteps = await getSteps();
+  }
+
+  Future<String> getSteps() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    String date = checkCurrentDate();
+
+    String? steps = prefs.getString(date);
+
+    if (steps == null) setSteps(int.parse('1'));
+
+    return steps ?? '1';
+  }
+
+  Future<void> setSteps(int steps) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    String date = checkCurrentDate();
+
+    try {
+      prefs.setString(date, steps.toString());
+    } catch (e) {
+      return Future.error('Error');
+    }
+
+    setTodaysSteps = steps.toString();
+  }
+
+  String checkCurrentDate() {
+    DateTime epoch = DateTime.now();
+
+    int date = epoch.day + epoch.month + epoch.year;
+
+    return date.toString();
+  }
+}
--- lib/ui/views/home/home_viewmodel.dart
+++ lib/ui/views/home/home_viewmodel.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 import 'dart:developer';
+import 'package:my_first_app/app/app.locator.dart';
+import 'package:my_first_app/services/step_storage_service.dart';
 import 'package:pedometer/pedometer.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:stacked/stacked.dart';
@@ -8,11 +10,22 @@ class HomeViewModel extends BaseViewModel {
   Stream<StepCount>? _stepCountStream;
   Stream<StepCount>? get stepCountStream => _stepCountStream;
 
+  final StepStorageService _stepStorageService = locator<StepStorageService>();
+
   Future? steps;
   int stepCount = 0;
 
-  void initialise() {
+  set setStepCount(String value) {
+    stepCount = int.parse(value);
+    notifyListeners();
+  }
+
+  void initialise() async {
     checkUserPermissions();
+
+    _stepStorageService.init();
+
+    setStepCount = await _stepStorageService.getSteps();
   }
 
   Future checkUserPermissions() async {
@@ -30,8 +43,9 @@ class HomeViewModel extends BaseViewModel {
     }
   }
 
-  void onStepCount(StepCount event) {
+  void onStepCount(StepCount event) async {
     stepCount++;
+    _stepStorageService.setSteps(stepCount);
     steps = Future.value(stepCount);
     notifyListeners();
   }
@@ -42,7 +56,6 @@ class HomeViewModel extends BaseViewModel {
 
   Future<void> initialisePedometer() async {
     _stepCountStream = Pedometer.stepCountStream;
-    notifyListeners();
     _stepCountStream!.listen(onStepCount).onError(handleStepError);
   }
 }
